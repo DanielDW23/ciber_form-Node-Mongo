0343f0e4ee3721f8069179cfb290189e5b6960b4
Daniel <dlopezg4@gmail.com>
2023-09-04 11:40:19 +0200

    funcionando, añadidas validades a email y resto de campos, añadido script nodemon

--- backend/model/FormModel.js
+++ backend/model/FormModel.js
@@ -1,10 +1,30 @@
 import mongoose from "mongoose";
+import isEmail from "validator/lib/isEmail.js";
 
 const FormLockSchema = new mongoose.Schema({
-  Email: { type: String },
-  Name: { type: String },
-  Subject: { type: String },
-  Message: { type: String }
+  Email: { 
+    type: String,
+    required  : [ true, 'El correo es obligatorio' ], 
+    validate: [ isEmail, 'Email invalido' ]
+     },
+  Name: {
+     type: String,
+     required  : [ true, 'El nombre es obligatorio' ],
+     maxlength : [ 50, 'El nombre no puede exceder los 50 caracteres'],
+     minlength : [ 3, 'El nombre debe contener 3 o más caracteres'] 
+    },
+  Subject: {
+     type: String,
+     required  : [ true, 'El asunto es obligatorio' ],
+     maxlength : [ 150, 'El asunto no puede exceder los 150 caracteres'],
+     minlength : [ 6, 'El asunto debe contener 3 o más caracteres'] 
+     },
+  Message: {
+     type: String,
+     required  : [ true, 'El mensaje es obligatorio' ],
+     maxlength : [ 600, 'El nombre no puede exceder los 600 caracteres'],
+     minlength : [ 6, 'El nombre debe contener 3 o más caracteres'] 
+     }
 });
 
 const FormLock = mongoose.model("Form", FormLockSchema);
