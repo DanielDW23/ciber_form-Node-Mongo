70f4682cb6ba7fe88ea2831c280044474ba26440
Daniel <dlopezg4@gmail.com>
2023-09-04 12:07:24 +0200

    añadido scape a los campos del formulario, funciona

--- backend/model/FormModel.js
+++ backend/model/FormModel.js
@@ -1,29 +1,34 @@
 import mongoose from "mongoose";
 import isEmail from "validator/lib/isEmail.js";
+import escape from "validator/lib/escape.js";
 
 const FormLockSchema = new mongoose.Schema({
   Email: { 
     type: String,
     required  : [ true, 'El correo es obligatorio' ], 
-    validate: [ isEmail, 'Email invalido' ]
+    validate: [ isEmail, 'Email invalido' ],
+    set: v => escape(v)
      },
   Name: {
      type: String,
      required  : [ true, 'El nombre es obligatorio' ],
      maxlength : [ 50, 'El nombre no puede exceder los 50 caracteres'],
-     minlength : [ 3, 'El nombre debe contener 3 o más caracteres'] 
+     minlength : [ 3, 'El nombre debe contener 3 o más caracteres'],
+     set: v => escape(v)
     },
   Subject: {
      type: String,
      required  : [ true, 'El asunto es obligatorio' ],
      maxlength : [ 150, 'El asunto no puede exceder los 150 caracteres'],
-     minlength : [ 6, 'El asunto debe contener 3 o más caracteres'] 
+     minlength : [ 6, 'El asunto debe contener 3 o más caracteres'],
+     set: v => escape(v)
      },
   Message: {
      type: String,
      required  : [ true, 'El mensaje es obligatorio' ],
      maxlength : [ 600, 'El nombre no puede exceder los 600 caracteres'],
-     minlength : [ 6, 'El nombre debe contener 3 o más caracteres'] 
+     minlength : [ 6, 'El nombre debe contener 3 o más caracteres'],
+     set: v => escape(v)
      }
 });
 
